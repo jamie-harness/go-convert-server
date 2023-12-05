b13922b2fcb80189b65a999757913ad73cacbc80
Jamie Li <tianyi.li@harness.io>
2023-12-05 15:03:24 -0500

    server working

---
+++ go.mod
@@ -0,0 +1,20 @@
+module github.com/jamie-harness/go-convert-server
+
+go 1.21.3
+
+require github.com/jamie-harness/go-convert v0.0.0-20231205194210-9252bd76cac4
+
+require (
+	dario.cat/mergo v1.0.0 // indirect
+	github.com/bmatcuk/doublestar v1.3.4 // indirect
+	github.com/buildkite/yaml v2.1.0+incompatible // indirect
+	github.com/docker/go-units v0.4.0 // indirect
+	github.com/drone/go-convert v0.0.0-20230919093251-7104c3bcc635 // indirect
+	github.com/drone/spec v0.0.0-20230919004456-7455b8913ff5 // indirect
+	github.com/ghodss/yaml v1.0.0 // indirect
+	github.com/google/subcommands v1.2.0 // indirect
+	github.com/gotidy/ptr v1.4.0 // indirect
+	golang.org/x/text v0.13.0 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
---
+++ main.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+
+	"github.com/drone/go-convert/convert/harness/downgrader"
+	"github.com/jamie-harness/go-convert/convert/jenkinsjson"
+)
+
+func uploadFile(w http.ResponseWriter, r *http.Request) {
+	// Maximum upload of 10 MB files
+	r.ParseMultipartForm(10 << 20)
+
+	// Get handler for filename, size and headers
+	file, handler, err := r.FormFile("myFile")
+	if err != nil {
+		fmt.Println("Error Retrieving the File")
+		fmt.Println(err)
+		return
+	}
+
+	defer file.Close()
+	// fmt.Printf("Uploaded File: %+v\n", handler.Filename)
+	// fmt.Printf("File Size: %+v\n", handler.Size)
+	// fmt.Printf("MIME Header: %+v\n", handler.Header)
+
+	// Create file
+	dst, err := os.Create(handler.Filename)
+	defer dst.Close()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// Copy the uploaded file to the created file on the filesystem
+	if _, err := io.Copy(dst, file); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	converter := jenkinsjson.New(
+		jenkinsjson.WithDockerhub(""),
+		jenkinsjson.WithKubernetes("", ""),
+	)
+	converted, err := converter.ConvertFile(handler.Filename)
+	if err != nil {
+		fmt.Println("error: ", err)
+	}
+	fmt.Println(converted)
+	d := downgrader.New(
+		downgrader.WithCodebase("", ""),
+		downgrader.WithDockerhub(""),
+		downgrader.WithKubernetes("", ""),
+		downgrader.WithName("default"),
+		downgrader.WithOrganization("default"),
+		downgrader.WithProject("default"),
+	)
+	converted, err = d.Downgrade(converted)
+	os.Stdout.Write(converted)
+	fmt.Fprintf(w, string(converted[:]))
+}
+
+func uploadHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "POST":
+		uploadFile(w, r)
+	}
+}
+
+func main() {
+	http.HandleFunc("/convert-to-harness", uploadHandler)
+	http.ListenAndServe(":8088", nil)
+}
